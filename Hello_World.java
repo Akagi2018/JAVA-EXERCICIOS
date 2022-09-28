17e500c8a47f530beb04b807e47f6942c19eec42
Akagi2018 <114014933+Akagi2018@users.noreply.github.com>
2022-09-28 13:15:40 -0300

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+class Projeto01 {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World.");	// TODO Auto-generated method stub
+	go();
+	System.out.println("fim");
+
+	}
+
+	private static void go() {
+		System.out.println("Entre no metodo go;");// TODO Auto-generated method stub
+		
+	}
+
+}
